7c9e87896cf9e278e031314c9268279491d593e8
dianajoan <dianajoanita900@gmail.com>
2019-06-14 20:26:23 +0300

    Added a master blade

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Staple Food a Restaurants Category Bootstrap Responsive website Template | Home :: w3layouts</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="keywords" content="Staple Food Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+			SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+		<!-- Custom Theme files -->
+		<link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+		<link href="css/style.css" type="text/css" rel="stylesheet" media="all">  
+		<link href="css/font-awesome.css" rel="stylesheet"> <!-- font-awesome icons -->
+		<link rel="stylesheet" href="css/owl.carousel.css" type="text/css" media="all"/> <!-- Owl-Carousel-CSS -->
+		<!-- //Custom Theme files --> 
+		<!-- js -->
+		<script src="js/jquery-2.2.3.min.js"></script>  
+		<!-- //js -->
+		<!-- web-fonts -->   
+		<link href="//fonts.googleapis.com/css?family=Berkshire+Swash" rel="stylesheet"> 
+		<link href="//fonts.googleapis.com/css?family=Yantramanav:100,300,400,500,700,900" rel="stylesheet">
+		<!-- //web-fonts -->
+	</head>
+	<body> 
+
+		@include('layouts.shared.navbar')
+
+		@yield('content')
+
+		@include('layouts.shared.footer')
+		
+	</body>
+</html>
