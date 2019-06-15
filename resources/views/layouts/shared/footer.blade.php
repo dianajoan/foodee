16dc079de6cbe9569136c0c7bf6c30850cc4c53f
dianajoan <dianajoanita900@gmail.com>
2019-06-15 09:09:46 +0300

    Modified the footer blade

---
+++ resources/views/layouts/shared/footer.blade.php
@@ -0,0 +1,49 @@
+<!-- footer -->
+	<div class="footer agileits-w3layouts">
+		<div class="container">
+			<div class="w3_footer_grids">
+				<div class="col-xs-6 col-sm-3 footer-grids w3-agileits">
+					<h3>company</h3>
+					<ul>
+						<li><a href="about.html">About Us</a></li>
+						<li><a href="contact.html">Contact Us</a></li>  
+						<li><a href="careers.html">Careers</a></li>  
+						<li><a href="help.html">Partner With Us</a></li>   
+					</ul>
+				</div> 
+				<div class="col-xs-6 col-sm-3 footer-grids w3-agileits">
+					<h3>help</h3>
+					<ul>
+						<li><a href="faq.html">FAQ</a></li> 
+						<li><a href="login.html">Returns</a></li>   
+						<li><a href="login.html">Order Status</a></li> 
+						<li><a href="offers.html">Offers</a></li> 
+					</ul>  
+				</div>
+				<div class="col-xs-6 col-sm-3 footer-grids w3-agileits">
+					<h3>policy info</h3>
+					<ul>  
+						<li><a href="terms.html">Terms & Conditions</a></li>  
+						<li><a href="privacy.html">Privacy Policy</a></li>
+						<li><a href="login.html">Return Policy</a></li> 
+					</ul>   
+				</div>
+				<div class="col-xs-6 col-sm-3 footer-grids w3-agileits">
+					<h3>Menu</h3> 
+					<ul>
+						<li><a href="menu.html">All Day Menu</a></li> 
+						<li><a href="menu.html">Lunch</a></li>
+						<li><a href="menu.html">Dinner</a></li>
+						<li><a href="menu.html">Flavours</a></li> 
+					</ul>  
+				</div> 
+				<div class="clearfix"> </div>
+			</div>
+		</div> 
+	</div>
+	<div class="copyw3-agile"> 
+		<div class="container">
+			<p>&copy; 2017 Staple Food. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
+		</div>
+	</div>
+	<!-- //footer -->
