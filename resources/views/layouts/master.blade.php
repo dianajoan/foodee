8a90c675209259b01c1d2babb4da314b53fc21de
dianajoan <dianajoanita900@gmail.com>
2019-06-15 09:41:11 +0300

    Modified the css and js in master blade

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,13 +8,13 @@
 			SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
 		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 		<!-- Custom Theme files -->
-		<link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
-		<link href="css/style.css" type="text/css" rel="stylesheet" media="all">  
-		<link href="css/font-awesome.css" rel="stylesheet"> <!-- font-awesome icons -->
-		<link rel="stylesheet" href="css/owl.carousel.css" type="text/css" media="all"/> <!-- Owl-Carousel-CSS -->
+		<link href="{!! asset('css/bootstrap.css') !!}" type="text/css" rel="stylesheet" media="all">
+		<link href="{!! asset('css/style.css') !!}" type="text/css" rel="stylesheet" media="all">  
+		<link href="{!! asset('css/font-awesome.css') !!}" rel="stylesheet"> <!-- font-awesome icons -->
+		<link rel="stylesheet" href="{!! asset('css/owl.carousel.css') !!}" type="text/css" media="all"/> <!-- Owl-Carousel-CSS -->
 		<!-- //Custom Theme files --> 
 		<!-- js -->
-		<script src="js/jquery-2.2.3.min.js"></script>  
+		<script src="{!! asset('js/jquery-2.2.3.min.js') !!}"></script>  
 		<!-- //js -->
 		<!-- web-fonts -->   
 		<link href="//fonts.googleapis.com/css?family=Berkshire+Swash" rel="stylesheet"> 
