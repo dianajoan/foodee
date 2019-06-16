23519b70dd869a2aaa2f83ffcfd4b61b290650d3
dianajoan <dianajoanita900@gmail.com>
2019-06-16 16:45:25 +0300

    Added more functioanlities in PageController

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,4 +11,34 @@ class PagesController extends Controller
 		return view('home');
 	}
 
+	public function about()
+	{
+		return view('about');
+	}
+
+	public function contact()
+	{
+		return view('contact');
+	}
+
+	public function careers()
+	{
+		return view('careers');
+	}
+
+	public function menu()
+	{
+		return view('menu');
+	}
+
+	public function offers()
+	{
+		return view('offers');
+	}
+
+	public function products()
+	{
+		return view('products');
+	}
+
 }
