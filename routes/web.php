57ef6faec43c3fc75fd95de458bad336b130e1f8
dianajoan <dianajoanita900@gmail.com>
2019-06-16 16:48:44 +0300

    Added more routes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,30 @@ Route::get('/', [
 	'as' 	=> 'welcome',
 	'uses' 	=> 'PagesController@home',
 ]);
+Route::get('/about', [
+	'as' 	=> 'about',
+	'uses' 	=> 'PagesController@about',
+]);
+Route::get('/contact', [
+	'as'  => 'contact',
+	'uses' => 'PagesController@contact',
+]);
+Route::get('/careers', [
+	'as'  => 'careers',
+	'uses' => 'PagesController@careers',
+]);
+Route::get('/menu', [
+	'as'  => 'menu',
+	'uses' => 'PagesController@menu',
+]);
+Route::get('/offers', [
+	'as'  => 'offers',
+	'uses' => 'PagesController@offers',
+]);
+Route::get('/products', [
+	'as'  => 'products',
+	'uses' => 'PagesController@products',
+]);
 
 Auth::routes();
 
