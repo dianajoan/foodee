97a5a7d40cc4b2df582fc9b52e4f88a2cee3f31e
dianajoan <dianajoanita900@gmail.com>
2019-06-16 17:39:27 +0300

    Added content in contact page

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,107 @@
+@extends('layouts.master')
+@section('title', 'Contact')
+
+@section('content')
+
+<!-- breadcrumb -->  
+	<div class="container">	
+		<ol class="breadcrumb w3l-crumbs">
+			<li><a href="{{ url('/') }}"><i class="fa fa-home"></i> Home</a></li> 
+			<li class="active">Contact Us</li>
+		</ol>
+	</div>
+	<!-- //breadcrumb -->
+	<!-- contact -->
+	<div id="contact" class="contact cd-section">
+		<div class="container">
+			<h3 class="w3ls-title">Contact us</h3>
+			<p class="w3lsorder-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit sheets containing sed </p> 
+			<div class="contact-row agileits-w3layouts">  
+				<div class="col-xs-6 col-sm-6 contact-w3lsleft">
+					<div class="contact-grid agileits">
+						<h4>DROP US A LINE </h4>
+						<form action="#" method="post"> 
+							<input type="text" name="Name" placeholder="Name" required="">
+							<input type="email" name="Email" placeholder="Email" required=""> 
+							<input type="text" name="Phone Number" placeholder="Phone Number" required="">
+							<textarea name="Message" placeholder="Message..." required=""></textarea>
+							<input type="submit" value="Submit" >
+						</form> 
+					</div>
+				</div>
+				<div class="col-xs-6 col-sm-6 contact-w3lsright">
+					<h6><span>Sed interdum </span>interdum accumsan nec purus ac orci finibus facilisis. In sit amet placerat nisl in auctor sapien. </h6>
+					<div class="address-row">
+						<div class="col-xs-2 address-left">
+							<span class="glyphicon glyphicon-home" aria-hidden="true"></span>
+						</div>
+						<div class="col-xs-10 address-right">
+							<h5>Visit Us</h5>
+							<p>Kampala, Uganda </p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="address-row w3-agileits">
+						<div class="col-xs-2 address-left">
+							<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+						</div>
+						<div class="col-xs-10 address-right">
+							<h5>Mail Us</h5>
+							<p><a href="mailto:info@example.com"> dianajoanita@900.com</a></p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="address-row">
+						<div class="col-xs-2 address-left">
+							<span class="glyphicon glyphicon-phone" aria-hidden="true"></span>
+						</div>
+						<div class="col-xs-10 address-right">
+							<h5>Call Us</h5>
+							<p>+256771991162</p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>  
+				</div>
+				<div class="clearfix"> </div>
+			</div>	
+		</div>	
+		<!-- map -->
+		<div class="map agileits">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.948805392833!2d-73.99619098458929!3d40.71914347933105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a27e2f24131%3A0x64ffc98d24069f02!2sCANADA!5e0!3m2!1sen!2sin!4v1479793484055"></iframe>
+		</div>
+		<!-- //map --> 
+	</div>
+	<!-- //contact -->   
+	<!-- subscribe -->
+	<div class="subscribe agileits-w3layouts"> 
+		<div class="container">
+			<div class="col-md-6 social-icons w3-agile-icons">
+				<h4>Keep in touch</h4>  
+				<ul>
+					<li><a href="#" class="fa fa-facebook icon facebook"> </a></li>
+					<li><a href="#" class="fa fa-twitter icon twitter"> </a></li>
+					<li><a href="#" class="fa fa-google-plus icon googleplus"> </a></li>
+					<li><a href="#" class="fa fa-dribbble icon dribbble"> </a></li>
+					<li><a href="#" class="fa fa-rss icon rss"> </a></li> 
+				</ul> 
+				<ul class="apps"> 
+					<li><h4>Download Our app : </h4> </li>
+					<li><a href="#" class="fa fa-apple"></a></li>
+					<li><a href="#" class="fa fa-windows"></a></li>
+					<li><a href="#" class="fa fa-android"></a></li>
+				</ul> 
+			</div> 
+			<div class="col-md-6 subscribe-right">
+				<h3 class="w3ls-title">Subscribe to Our <br><span>Newsletter</span></h3>  
+				<form action="#" method="post"> 
+					<input type="email" name="email" placeholder="Enter your Email..." required="">
+					<input type="submit" value="Subscribe">
+					<div class="clearfix"> </div> 
+				</form> 
+				<img src="images/i1.png" class="sub-w3lsimg" alt=""/>
+			</div>
+			<div class="clearfix"> </div> 
+		</div>
+	</div>
+	<!-- //subscribe --> 
+@endsection
