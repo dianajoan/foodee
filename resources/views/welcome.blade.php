d40bc8d11da12cefc9717a037e08c5610811a2c2
dianajoan <dianajoan900@gmail.com>
2019-09-23 14:44:23 +0300

    Added favicon icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css'>
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="img/favicon.ico">
 
         <title>{{ config('app.name') }} | Welcome</title>
 
