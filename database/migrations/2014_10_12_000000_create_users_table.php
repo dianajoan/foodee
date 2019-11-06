ca1fb808c90ce5efb4fe4b732f2411edc32cb0da
dianajoan <dianajoan900@gmail.com>
2019-11-06 19:42:25 +0300

    added more tables for users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,17 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            
+            $table->string('role')->nullable()->default('user');
+            $table->string('username')->nullable()->unique();
+            $table->string('telephone')->nullable();
+            $table->string('occupation')->nullable();
+            $table->string('location')->default('Kampala (Central), Uganda')->nullable();
+            $table->string('gender')->default('Unknown')->nullable();
+            $table->string('profile_image')->default('profile.png')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->string('nationality')->default('Ugandan')->nullable(); 
+            $table->string('status')->default('Active')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
