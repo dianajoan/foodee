389bc89e1d285865c8bed97c11ba524f69551097
dianajoan <dianajoan900@gmail.com>
2019-11-06 17:42:50 +0300

    installing zizaco entrust

--- config/app.php
+++ config/app.php
@@ -164,7 +164,7 @@ return [
 
         /*
          * Package Service Providers...
-         */
+         */Zizaco\Entrust\EntrustServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +225,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
 
     ],
 
