3a39c7e244b10db303e347d6ad801c71e44a7c44
dianajoan <dianajoan900@gmail.com>
2019-11-11 19:32:09 +0300

    adding terms of the project

--- resources/views/web/more/terms.blade.php
+++ resources/views/web/more/terms.blade.php
@@ -3,27 +3,77 @@
 
 @section('content')
 
-	<!-- privacy-page -->
-	<div class="privacy about">
-		<div class="container"> 
-			<h3 class="w3ls-title w3ls-title1">Terms & Conditions</h3>  
-			<div class="privacy-w3row">
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ornare turpis ut porta aliquet. Morbi quis dictum lacus. Donec scelerisque lacus nec lobortis tempor. Morbi orci arcu, ullamcorper ut arcu vel, ultricies congue lacus. Phasellus porta cursus tempor. Nunc ultricies sapien nec est ornare pulvinar. Vivamus mollis faucibus purus sed blandit. Praesent fermentum dapibus risus id rutrum. Aliquam rutrum vel nulla eu fringilla.</p>
-				<p>Nunc ornare turpis ut porta aliquet. Morbi quis dictum lacus. Donec scelerisque lacus nec lobortis tempor. Morbi orci arcu, ullamcorper ut arcu vel, ultricies congue lacus. Phasellus porta cursus tempor. Nunc ultricies sapien nec est ornare pulvinar. Vivamus mollis faucibus purus sed blandit. Praesent fermentum dapibus risus id rutrum. Aliquam rutrum vel nulla eu fringilla. Curabitur efficitur justo a est tempor, pretium accumsan leo sagittis. Ut imperdiet sapien velit, a auctor neque laoreet sed. Praesent ornare nulla sit amet ullamcorper vulputate. Suspendisse vitae magna finibus, egestas eros quis, ornare risus. Duis magna ex, ultrices quis tellus non, commodo eleifend odio. Donec et faucibus risus. Aliquam laoreet justo sapien. </p>
-				<h5>PRICES AND PAYMENT</h5>  
-				<p>Maecenas malesuada leo dui, at venenatis ligula sodales ut. Cras a feugiat orci. </p>
-				<p>Pellentesque metus enim, aliquet eu felis sed, pretium ultrices ante convallis nec turpis.</p>
-				<p>Phasellus imperdiet nibh et lorem tincidunt aliquam. Integer elementum purus non venenatis volutpat. Curabitur sit amet neque sed arcu pretium iaculis. Phasellus posuere egestas tortor a molestie. Nulla sed urna dignissim, maximus magna et, elementum metus. Ut dolor lorem, mattis eu consectetur non, euismod quis nulla. </p>			
-				<h5>SECURITY</h5>  
-				<p>Phasellus imperdiet nibh et lorem tincidunt aliquam. Integer elementum purus non venenatis volutpat. Curabitur sit amet neque sed arcu pretium iaculis. Phasellus posuere egestas tortor a molestie. Nulla sed urna dignissim, maximus magna et, elementum metus. Ut dolor lorem, mattis eu consectetur non, euismod quis nulla. </p>
-				<p>Donec scelerisque lacus nec lobortis tempor donec et faucibus risus. Aliquam laoreet justo sapien. </p>
-				<p>Pellentesque quis sagittis est. Nam placerat ante at nunc ornare, sed faucibus odio interdum. Nam viverra, purus ac ullamcorper convallis, lorem erat consequat enim, at pharetra magna. </p>
-				<h5>ORDER CONDITIONS, DELIVERY</h5>  
-				<p>Maecenas malesuada leo dui, at venenatis ligula sodales ut. Cras a feugiat orci. </p>
-				<p>Pellentesque metus enim, aliquet eu felis sed, pretium ultrices ante convallis nec turpis.</p>
+	<!-- Breadcrumbs -->
+      <section class="breadcrumbs-custom-inset">
+        <div class="breadcrumbs-custom context-dark bg-overlay-46">
+          <div class="container">
+            <h2 class="breadcrumbs-custom-title">Terms & Conditions of the Restaurant</h2>
+            <ul class="breadcrumbs-custom-path">
+              <li><a href="index.html">Home</a></li>
+              <li class="active">Terms & Conditions</li>
+            </ul>
+          </div>
+          <div class="box-position" style="background-image: url({{ asset('images/bg-breadcrumbs.jpg' ) }});"></div>
+        </div>
+      </section>
+
+	<!-- terms-page -->
+	<section class="section section-md section-first bg-default">
+		<div class="privacy about">
+			<div class="container">   
+				<div class="privacy-w3row">
+					<p>PLEASE READ THIS TERMS AND CONDITIONS AGREEMENT ("AGREEMENT") CAREFULLY. THIS AGREEMENT IS A LEGAL CONTRACT BETWEEN YOU ("USER") AND MEREDITH CORPORATION ("MEREDITH," "COMPANY," "WE," OR "US").</p>
+
+					<p>SECTION 15 OF THIS AGREEMENT IS AN ARBITRATION CLAUSE THAT REQUIRES MOST DISPUTES BETWEEN US TO BE RESOLVED THROUGH BINDING AND FINAL ARBITRATION INSTEAD OF IN COURT. SEE SECTION 15 FOR MORE INFORMATION REGARDING THIS ARBITRATION CLAUSE, AND HOW TO OPT OUT. By accessing or using any website with an authorized link to this Agreement ("Website"), registering an account or accessing or using any content, information, services, features or resources available or enabled via the Website (collectively with the Website, the "Services"), clicking on a button or taking another action to signify your acceptance of this Agreement, you: (1) agree to be bound by this Agreement and any future amendments and additions to this Agreement as published through the Services; (2) represent you are of legal age in your jurisdiction of residence to form a binding contract; and (3) represent that you have the authority to enter into this Agreement personally and, if applicable, on behalf of any company, organization or other legal entity on whose behalf you use the Services. Except as otherwise provided herein, if you do not agree to be bound by this Agreement, you may not access or use the Services. </p>
+
+					<br>
+
+					<h5>Registration</h5>
+
+					<p>When registering an account for the Services ("Account"), you agree to provide only true, accurate, current and complete information requested by the registration form (the "Registration Data") and to promptly update the Registration Data thereafter as necessary. You represent that you are not barred from using the Services under any applicable law and that you will be responsible for all activities that occur under your Account. You agree to monitor your Account to restrict its use by minors and other unauthorized users and agree not to share your Account or password with anyone. You further agree to notify Meredith immediately of any unauthorized use of your password or any other breach of the security of your Account and to exit from your Account at the end of each session. You agree not to create an Account using a false identity or alias or if you previously have been banned from using any of the Services. You further agree that you will not maintain more than one Account for the same Meredith service at any given time. Meredith reserves the right to remove or reclaim any usernames at any time and for any reason. You acknowledge and agree that you have no ownership or other property interest in your Account and that all rights in and to your Account are owned by and inure to the benefit of Meredith.</p>
+
+					<br>
+
+					<h5>Order Acceptance</h5>  
+					<p>Each part of any order that you submit to Meredith constitutes an offer to purchase. If you do not receive a message from Meredith confirming receipt of your order, please contact our Customer Service department before re-entering your order. Meredith's confirmation of receipt of your order does not constitute Meredith's acceptance of your order. Meredith is only deemed to have accepted your order once (i) the product(s) you ordered (each, a "Product") have been shipped (if the Product is a one-time order) or (ii) the initial Product for a subscription has been delivered (if the Product is subject to a subscription for the delivery of periodicals or other tangible goods) or (iii) you are granted access to the paid content or resources of a Product consisting of a digital subscription or membership).</p>
+
+					<br>
+
+					<h5>Order Issues</h5>  
+					<p>Although we strive to accept all valid orders, Meredith reserves the right to deny any order for any reason, including if: (i) we discover an error in pricing and/or other information about the Product or receive insufficient or erroneous billing, payment, and/or shipping information, (ii) we suspect an order has been placed using stolen payment card information or otherwise appears to be connected to fraud, or (iii) the ordered Product is unavailable due to discontinuance or otherwise. We may also refuse any order that is connected with a previous payment dispute.</p>
+
+					<br>
+
+					<h5>Order Cancellation</h5>
+					<p>If any Product is discontinued or otherwise becomes unavailable, Meredith reserves the right to (i) cancel your order and provide you a refund for the amount paid for the Product (if the Product is a one-time order), (ii) substitute the Product with a similar Product (if the Product is subject to a subscription) or (iii) issue you a pro rata refund.</p>
+
+					<br>
+
+					<h5>Restrictions on Resale</h5>
+					<p>To protect the intellectual property rights of Meredith and its licensors and suppliers, any resale of Products for personal and/or business profit is strictly prohibited. Meredith reserves the right to decline any order that we deem to possess characteristics of reselling.</p>
+
+					<br>
+
+					<h5>Payment</h5>  
+					<p>You agree to pay all fees or charges to your Account in accordance with the fees, charges and billing terms in effect at the time a fee or charge is due and payable. You also agree to pay all applicable taxes. You must provide Meredith with valid payment information in connection with your orders. By providing Meredith with your payment information, you agree that (i) Meredith is authorized to immediately invoice your Account for all fees and charges due and payable to Meredith hereunder, (ii) Meredith is authorized to share any payment information and instructions required to complete the payment transactions with its third-party payment service providers (e.g., credit card transaction processing, merchant settlement, and related services), and (iii) no additional notice or consent is required for the foregoing authorizations. You agree to immediately notify Meredith of any change in your payment information. Meredith reserves the right at any time to change its prices and billing methods. If payment cannot be charged to your payment card or your payment is returned for any reason, Meredith reserves the right to either suspend or terminate your access to the upaid-for services.</p>
+
+					<br>
+
+					<h5>Automatic Renewal</h5>
+					<p>If your order is subject to automatic renewal, you will be notified during the order process. The automatic renewal and cancellation terms provided during the order process are hereby incorporated into this Agreement.</p>
+
+					<h5>Refunds</h5>
+					<p>Except as set forth in Section 2.3 or in any separate refund policy posted on the Services, all fees are non-refundable.</p>
+
+					<h5>Discounts</h5>
+					<p>We may, in our sole discretion, create discounts and promotional codes that may be redeemed for credit in your Account, or other features or benefits, subject to any additional terms that we establish on a per promotional code basis ("Promo Codes"). Promo Codes may only be used once per person. Only Promo Codes sent to you through official Meredith communications channels are valid. You agree that Promo Codes: (i) must be used for the intended audience and purpose, and in a lawful manner; (ii) may not be duplicated, sold, or transferred in any manner, or made available to the general public (whether posted to a public forum or otherwise), unless expressly permitted by us; (iii) may be disabled by us at any time for any reason without liability to us; (iv) may only be used pursuant to the specific terms that we establish for such Promo Code; (v) are not redeemable for cash; and (vi) may expire prior to your use. From time to time, we may offer special promotions (such as free premiums with purchase), associated with some of our products or Services. All promotional offers, inlcuding premium offers, may be discontinued at any time, without prior notice, at our discretion and are only available while supplies last or for the duration noted.  Unless otherwise stated in the offer, shipment of premium items may take 6-12 weeks and shipping fees may apply.</p>
+
+
+
+				</div>
 			</div>
 		</div>
-	</div>
-	<!-- //privacy-page --> 
+	</section>
+	<!-- //terms-page --> 
 <a class="section section-banner" href="https://www.templatemonster.com/intense-multipurpose-html-template.html" style="background-image: url({{ asset('images/banner/background-03-1920x310.jpg') }} ); background-image: -webkit-image-set( url({{ asset('images/banner/background-03-1920x310.jpg 1x') }} ), url({{ asset('images/banner/background-03-3840x620.jpg 2x') }} )"><img src="{{ asset('images/banner/foreground-03-1600x310.png') }}" srcset="{{ asset('images/banner/foreground-03-1600x310.png 1x') }}, {{ asset('images/banner/foreground-03-3200x620.png 2x') }}" alt="" width="1600" height="310"></a>
 @endsection
