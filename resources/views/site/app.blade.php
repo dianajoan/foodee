91eb5c384833c4580dd0dd83b75c3890d0f5eea9
dianajoan <dianajoan900@gmail.com>
2019-12-06 21:32:02 +0300

    added more content in the site

--- resources/views/site/app.blade.php
+++ resources/views/site/app.blade.php
@@ -3,6 +3,7 @@
   <head>
     <title>@yield('title') - {{ config('app.name') }}</title>
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
     @include('site.partials.styles')
--- resources/views/site/auth.blade.php
+++ resources/views/site/auth.blade.php
@@ -4,6 +4,7 @@
 		<title>@yield('title') | {{ config('app.name') }}</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta charset="UTF-8">
+		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--===============================================================================================-->	
 		<link rel="icon" type="image/png" href="{{ asset('images/icons/favicon.ico') }}">
