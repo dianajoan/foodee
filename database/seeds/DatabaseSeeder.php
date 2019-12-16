55f49dd2c427c102c0c1aebfbf739108d99e8097
dianajoan <dianajoan900@gmail.com>
2019-12-16 21:41:31 +0300

    updating seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
         $this->call(RolesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call(ProductTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
 
